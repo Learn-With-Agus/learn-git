c0a76a901ddfa5f85d53ef8bd029fc899e288edb
agusTrih <ant3dong@gmail.com>
2020-06-30 08:19:30 +0700

    oke learn part 1

--- src/App.js
+++ src/App.js
@@ -3,33 +3,10 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-    const myName = "agus";
-    const strongName = <strong>Agus</strong>;
-    let numberA = 8;
-    let numberB = 80;
-    let hasil = numberA + numberB;
     return (
-        <div class="learn-operation">
-            <h1>
-                Halo {myName} hari ini kita belajar menghitung {numberA} +{" "}
-                {numberB} = {hasil}{" "}
-            </h1>
-            <h1>
-                Halo {myName} hari ini kita belajar menghitung {numberA} +{" "}
-                {numberB} = {hasil}{" "}
-            </h1>
-            <h1>
-                Halo {myName} hari ini kita belajar menghitung {numberA} +{" "}
-                {numberB} = {hasil}{" "}
-            </h1>
-            <h1>
-                Halo {myName} hari ini kita belajar menghitung {numberA} +{" "}
-                {numberB} = {hasil}{" "}
-            </h1>
-            <h1>
-                Halo {myName} hari ini kita belajar menghitung {numberA} +{" "}
-                {numberB} = {hasil}{" "}
-            </h1>
+        <div class="Parentbox">
+            <h1>agus</h1>
+            <img src="design.jpg"></img>
         </div>
     );
 }
